717991601a539b7e13f27335d47a6c6a4200dd00
Rocket-Octane <93807708+Rocket-Octane@users.noreply.github.com>
2022-04-04 21:01:13 -0400

    Add files via upload

--- ArTwitter_room.js
+++ ArTwitter_room.js
@@ -13,6 +13,19 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
+user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+function addRoom(){
+      room_name = document.getElementById("room_name").value;
+      firebase.database().ref("/").child(room_name).update({
+            purpose: "adding room name"
+      });
+      console.log(room_name);
+      localStorage.setItem("room_name", room_name);
+      window.location = "kwitter_page.html";
+}
+
 function getData() {
   firebase.database().ref("/").on('value', function (snapshot) {
     document.getElementById("output").innerHTML = "";
@@ -20,9 +33,23 @@ function getData() {
       childKey = childSnapshot.key;
       Room_names = childKey;
       //Start code
-
+      console.log("Room names: " + Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#" + Room_names + "</div><hr>";
+      document.getElementById("output").innerHTML += row;
       //End code
     });
   });
 }
 getData();
+
+function redirectToRoomName(name){
+  console.log(name);
+  localStorage.setItem("room_name", name);
+  window.location = "kwitter_page.html";
+}
+
+function logout(){
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("room_name");
+  window.location = "index.html";
+}
