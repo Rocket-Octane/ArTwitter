de2c63dbbb881d34b12864f70ab555ccd762c67d
Rocket-Octane <93807708+Rocket-Octane@users.noreply.github.com>
2022-04-04 21:51:24 -0400

    Add files via upload

--- ArTwitter_room.js
+++ ArTwitter_room.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 user_name = localStorage.getItem("user_name");
-document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+document.getElementById("user_name_label").innerHTML = "Welcome " + user_name + "!";
 
 function addRoom(){
       room_name = document.getElementById("room_name").value;
